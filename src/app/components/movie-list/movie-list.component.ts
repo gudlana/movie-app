5a053972364ae3421b48ec30e678096ec7481e78
Svitlana Hudym <svitlana.hudym@gmail.com>
2024-06-17 20:35:28 +0300

    fixed bags on feedback

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MovieCardComponent } from '../movie-card/movie-card.component';
 import { movies } from '../../entities/moviesData';
+import { Movie } from '../../entities/moviesIData';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,20 +12,24 @@ import { CommonModule } from '@angular/common';
   styleUrl: './movie-list.component.scss',
 })
 export class MovieListComponent {
-  public movies = movies;
-  public favoritesList: string[] = [];
-  public watchList: string[] = [];
+  movies: Movie[] = movies;
+  public favoritesList: Movie[] = [];
+  public watchList: Movie[] = [];
+
+  private addToList(list: Movie[], id: number) {
+    const movieToList = list.find((movie) => movie.id === id);
+    if (movieToList) return list;
+    const addedMovie = this.movies.find((movie) => movie.id === id);
+    if (addedMovie) {
+      return [...list, addedMovie];
+    }
+    return list;
+  }
 
   public addToFavoritesList(id: number): void {
-    let movie = this.movies.find((movie) => movie.id === id);
-    if (movie) {
-      this.favoritesList.push(movie.title);
-    }
+    this.favoritesList = this.addToList(this.favoritesList, id);
   }
   public addToWatchList(id: number): void {
-    let movie = this.movies.find((movie) => movie.id === id);
-    if (movie) {
-      this.watchList.push(movie.title);
-    }
+    this.watchList = this.addToList(this.watchList, id);
   }
 }
